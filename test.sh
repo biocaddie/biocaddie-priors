cfc957e0903e48853046cce6a6adfab40deb7fea
Garrick Sherman <gsherma2@illinois.edu>
2017-07-25 16:51:40 -0700

    test.sh is correct for doc priors

--- test.sh
+++ test.sh
@@ -10,17 +10,8 @@ TEST_PASSWORD="changeme"
 SEARCH_INDEX="biocaddie"
 SEARCH_TYPE="dataset"
 TEST_QUERY="multiple+sclerosis"
-EXPAND_ON="biocaddie"
-STOP_LIST="a+an+the+and+or+of+from+on+was+to+is+-+were+at+as+we"
 
 # Override additional parameters here
-ADDITIONAL_ARGS="&fbTerms=20&fbDocs=50"
+ADDITIONAL_ARGS="&fbDocs=50"
 
-# If search was specified, use the _esearch endpoint
-if [ "$1" == "search" ]; then 
-    curl -u "${TEST_USERNAME}:${TEST_PASSWORD}" ${TEST_HOST}:${TEST_PORT}/${SEARCH_INDEX}/${SEARCH_TYPE}/_esearch'?pretty&query='${TEST_QUERY}${ADDITIONAL_ARGS}'&stoplist='${STOP_LIST}'&expandOn='${EXPAND_ON}
-    exit 0
-fi
-
-# Otherwise, just run Rocchio and return the expanded query
-curl -u "${TEST_USERNAME}:${TEST_PASSWORD}" ${TEST_HOST}:${TEST_PORT}/${EXPAND_ON}/${SEARCH_TYPE}/_expand'?pretty'${ADDITIONAL_ARGS}'&query='${TEST_QUERY}
+curl -u "${TEST_USERNAME}:${TEST_PASSWORD}" ${TEST_HOST}:${TEST_PORT}/${SEARCH_INDEX}/${SEARCH_TYPE}/_priorsearch'?pretty&query='${TEST_QUERY}${ADDITIONAL_ARGS}
